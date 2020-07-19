d816e3bb1dcfb8e9c35b8f9fa596488888d9979a
KunMinX <kunminx@gmail.com>
2020-07-19 14:34:23 +0800

    update comment

--- strictdatabinding/src/main/java/com/kunminx/architecture/ui/page/DataBindingActivity.java
+++ strictdatabinding/src/main/java/com/kunminx/architecture/ui/page/DataBindingActivity.java
@@ -77,20 +77,20 @@ public abstract class DataBindingActivity extends AppCompatActivity {
         return mBinding;
     }
 
-    /**
-     * TODO tip: DataBinding 严格模式：
-     * 将 DataBinding 实例限制于 base 页面中，默认不向子类暴露，
-     * 通过这样的方式，来彻底解决 视图调用的一致性问题，
-     * 如此，视图刷新的安全性将和基于函数式编程的 Jetpack Compose 持平。
-     * <p>
-     * 如果这样说还不理解的话，详见 https://xiaozhuanlan.com/topic/9816742350 和 https://xiaozhuanlan.com/topic/2356748910
-     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         initViewModel();
         DataBindingConfig dataBindingConfig = getDataBindingConfig();
+
+        //TODO tip: DataBinding 严格模式：
+        // 将 DataBinding 实例限制于 base 页面中，默认不向子类暴露，
+        // 通过这样的方式，来彻底解决 视图调用的一致性问题，
+        // 如此，视图刷新的安全性将和基于函数式编程的 Jetpack Compose 持平。
+
+        // 如果这样说还不理解的话，详见 https://xiaozhuanlan.com/topic/9816742350 和 https://xiaozhuanlan.com/topic/2356748910
+
         ViewDataBinding binding = DataBindingUtil.setContentView(this, dataBindingConfig.getLayout());
         binding.setLifecycleOwner(this);
         binding.setVariable(dataBindingConfig.getVmVariableId(), dataBindingConfig.getStateViewModel());
